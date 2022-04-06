8e3ba557cbb495faa521b6195aec7d28ba78b37c
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-04-06 11:30:28 -0500

    APIs functiuoning in backend

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,28 @@
 var searchBtnEl = document.getElementById('searchBtn');
 
-searchBtnEl.addEventListener('click', function() {
+searchBtnEl.addEventListener('click', function(event) {
+    event.preventDefault();
     var searchInputEl = document.getElementById('searchInput');
     var userInput = searchInputEl.value;
     fetchSearch(userInput);
 });
 function fetchSearch(city) {
-    var cityAPI ='https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=f67e4c0b75e73789ccfab3486194bd2f';
+    console.log(city);
+    var cityAPI =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f67e4c0b75e73789ccfab3486194bd2f`;
     fetch(cityAPI).then(function(response) {
         return response.json()
     }).then(function(data) {
         console.log(data)
-        //pull lat and long from data data.lan
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        fetchWeather(lat, lon);
     }).catch(function(error) {
         console.log(error)
     })
 }
-var weatherAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=f67e4c0b75e73789ccfab3486194bd2f';
+function fetchWeather(lat, lon) {
+    var weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=f67e4c0b75e73789ccfab3486194bd2f`;
+    fetch(weatherAPI).then(response => response.json()).then(function(data) {
+        console.log(data)
+    })
+}
