561859f68b6d3cd52f49b020228de02d731bbed2
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-04-06 12:13:03 -0500

    staged for jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,28 @@ searchBtnEl.addEventListener('click', function(event) {
     fetchSearch(userInput);
 });
 function fetchSearch(city) {
-    console.log(city);
     var cityAPI =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f67e4c0b75e73789ccfab3486194bd2f`;
     fetch(cityAPI).then(function(response) {
         return response.json()
     }).then(function(data) {
-        console.log(data)
         var lat = data.coord.lat;
         var lon = data.coord.lon;
-        fetchWeather(lat, lon);
+        return fetchWeather(lat, lon);
+    }).then(function(forecastWeather) {
+        renderForecast(forecastWeather);
     }).catch(function(error) {
         console.log(error)
     })
 }
 function fetchWeather(lat, lon) {
-    var weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=f67e4c0b75e73789ccfab3486194bd2f`;
-    fetch(weatherAPI).then(response => response.json()).then(function(data) {
-        console.log(data)
-    })
+    var weatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=f67e4c0b75e73789ccfab3486194bd2f`;
+    return fetch(weatherAPI).then(response => response.json()).then((data) => {
+        return data;
+    }).catch(function(error) {
+        console.log(error)
+    });
 }
+function renderForecast(forecastWeather) {
+    var forecastEl = document.getElementById('forecast');
+    console.log(forecastWeather);
+};
